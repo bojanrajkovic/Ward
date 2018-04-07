d4caab109e8def8f57bfaabcd99743412f1bdb36
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2018-04-07 08:32:19 -0400

    Dns: Add code to parse DNS header, test it.

--- src/Aelfweard.Dns/Header.cs
+++ src/Aelfweard.Dns/Header.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 
 namespace Aelfweard.Dns
 {
@@ -21,6 +22,59 @@ namespace Aelfweard.Dns
         public ushort TotalAnswerRecords { get; }
         public ushort TotalAuthorityRecords { get; }
         public ushort TotalAdditionalRecords { get; }
+
+        public static Header ParseFromBytes(byte[] messageBytes, int offset)
+        {
+            var memStream = new MemoryStream(messageBytes);
+            memStream.Position = offset;
+            return ParseFromStream(memStream);
+        }
+
+        public static Header ParseFromStream(Stream messageStream)
+        {
+            var reader = new BinaryReader(messageStream);
+            var needsSwap = BitConverter.IsLittleEndian;
+
+            ushort SwapUInt16(ushort x) =>
+                needsSwap ? (ushort)((ushort)((x & 0xff) << 8) | ((x >> 8) & 0xff)) : x;
+
+            var id = SwapUInt16(reader.ReadUInt16());
+
+            var flags = SwapUInt16(reader.ReadUInt16());
+            var query = (flags & 0b1000_0000_0000_0000) == 0;
+            var opcode = (Opcode)(flags & 0b0111_1000_0000_0000);
+            var authoritative = (flags & 0b0000_0100_0000_0000) != 0;
+            var truncated = (flags & 0b0000_0010_0000_0000) != 0;
+            var recurse = (flags & 0b0000_0001_0000_0000) != 0;
+            var recursionAvailable = (flags & 0b0000_0000_1000_0000) != 0;
+            var z = (flags & 0b0000_0000_0100_0000) != 0;
+            var authenticated = (flags & 0b0000_0000_0010_0000) != 0;
+            var checkingDisabled = (flags & 0b0000_0000_0001_0000) != 0;
+            var returnCode = (ReturnCode)(flags & 0b0000_0000_0000_1111);
+
+            var totalQuestions = SwapUInt16(reader.ReadUInt16());
+            var totalAnswerRecords = SwapUInt16(reader.ReadUInt16());
+            var totalAuthorityRecords = SwapUInt16(reader.ReadUInt16());
+            var totalAdditionalRecords = SwapUInt16(reader.ReadUInt16());
+
+            return new Header(
+                id,
+                query,
+                opcode,
+                authoritative,
+                truncated,
+                recurse,
+                recursionAvailable,
+                z,
+                authenticated,
+                checkingDisabled,
+                returnCode,
+                totalQuestions,
+                totalAnswerRecords,
+                totalAuthorityRecords,
+                totalAdditionalRecords
+            );
+        }
 
         public Header(
             ushort? id,
--- test/Aelfweard.Dns.Tests/HeaderTests.cs
+++ test/Aelfweard.Dns.Tests/HeaderTests.cs
@@ -5,12 +5,62 @@ namespace Aelfweard.Dns.Tests
 {
     public class HeaderTests
     {
-        const string testMessage = "qqqBgAABAAEAAAAAB2V4YW1wbGUDY29tAAABAAHADAABAAEAADu8AARduNgi";
+        /// <summary>
+        /// A Base64-encoded DNS response from 8.8.8.8, having been asked for example.com.
+        /// </summary>
+        const string responseMessage = "qqqBgAABAAEAAAAAB2V4YW1wbGUDY29tAAABAAHADAABAAEAADu8AARduNgi";
+
+        /// <summary>
+        /// A Base64-encoded DNS query message, captured from Dig 9.11.3 on Fedora 29.
+        /// The request is for google.com, and was sent as `dig google.com`
+        /// </summary>
+        const string requestMessage = "HfoBIAABAAAAAAABBmdvb2dsZQNjb20AAAEAAQAAKRAAAAAAAAAMAAoACJOxHvPq368a";
 
         [Fact]
-        public void Can_parse_header()
+        public void Can_parse_header_from_request()
         {
-            var message = Convert.FromBase64String(testMessage);
+            var message = Convert.FromBase64String(requestMessage);
+            var header = Header.ParseFromBytes(message, 0);
+
+            Assert.Equal(0x1dfa, header.Id);
+            Assert.True(header.Query);
+            Assert.Equal(Opcode.Query, header.Opcode);
+            Assert.False(header.Truncated);
+            Assert.True(header.Recurse);
+            Assert.False(header.RecursionAvailable);
+            Assert.False(header.Z);
+            Assert.True(header.Authenticated);
+            Assert.False(header.CheckingDisabled);
+            Assert.Equal(ReturnCode.NoError, header.ReturnCode);
+
+            Assert.Equal(1, header.TotalQuestions);
+            Assert.Equal(0, header.TotalAnswerRecords);
+            Assert.Equal(0, header.TotalAuthorityRecords);
+            Assert.Equal(1, header.TotalAdditionalRecords);
+        }
+
+        [Fact]
+        public void Can_parse_header_from_response()
+        {
+            var message = Convert.FromBase64String(responseMessage);
+            var header = Header.ParseFromBytes(message, 0);
+
+            Assert.Equal(0xAAAA, header.Id);
+            Assert.False(header.Query);
+            Assert.Equal(Opcode.Query, header.Opcode);
+            Assert.False(header.Authoritative);
+            Assert.False(header.Truncated);
+            Assert.True(header.Recurse);
+            Assert.True(header.RecursionAvailable);
+            Assert.False(header.Z);
+            Assert.False(header.Authenticated);
+            Assert.False(header.CheckingDisabled);
+            Assert.Equal(ReturnCode.NoError, header.ReturnCode);
+
+            Assert.Equal(1, header.TotalQuestions);
+            Assert.Equal(1, header.TotalAnswerRecords);
+            Assert.Equal(0, header.TotalAuthorityRecords);
+            Assert.Equal(0, header.TotalAdditionalRecords);
         }
     }
 }
