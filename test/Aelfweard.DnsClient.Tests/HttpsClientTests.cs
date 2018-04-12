25fa762d575521f434bfc813895af299d811b9ab
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2018-04-12 02:21:05 -0400

    DnsClient: DoH client + tests.

---
+++ test/Aelfweard.DnsClient.Tests/HttpsClientTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+using Aelfweard.Dns;
+using Aelfweard.Dns.Records;
+using Xunit;
+
+namespace Aelfweard.DnsClient.Tests
+{
+    public class HttpsClientTests
+    {
+        [Fact]
+        public async Task Can_resolve_A_records_via_HTTPS()
+        {
+            var client = new HttpsDnsClient(IPAddress.Parse("1.1.1.1"), 443, "cloudflare-dns.com");
+            var resolve = await client.ResolveAsync("example.com", Dns.Type.A, Class.Internet);
+
+            Assert.NotNull(resolve);
+            Assert.Single(resolve.Results);
+
+            var a = Assert.IsType<AddressRecord>(resolve.Results[0]);
+            Assert.Equal("93.184.216.34", a.Address.ToString());
+            Assert.Equal(Class.Internet, a.Class);
+        }
+    }
+}
