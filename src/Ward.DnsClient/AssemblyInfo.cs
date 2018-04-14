7800899c141ef5f2c313385e251c6c5b3da1bb29
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2018-04-14 15:03:23 -0400

    CI: Fix AssemblyInfo files so CI can properly version.

--- src/Ward.DnsClient/AssemblyInfo.cs
+++ src/Ward.DnsClient/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Ward.Dns.Tests")]
+[assembly: InternalsVisibleTo("Ward.DnsClient.Tests")]
 [assembly: AssemblyCompany("Code, Rinse, Repeat")]
