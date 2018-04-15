a7e7d12bb0324211acad923ab14538f8b15b6d57
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2018-04-15 11:11:19 -0400

    Tests: Update message tests to use TOML test cases.

--- test/Ward.Dns.Tests/Assertions.cs
+++ test/Ward.Dns.Tests/Assertions.cs
@@ -1,11 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 using Nett;
+using Ward.Dns;
 using Xunit;
 
 namespace Ward.Dns.Tests
 {
-    static class Assertions
+    static class Helpers
     {
-        public static void AssertHeader(Header header, TomlTable expectedHeader)
+        public static void ForEach<T>(this IList<T> list, Action<T, int> callback)
+        {
+            for (var i = 0; i < list.Count; i++)
+                callback(list[i], i);
+        }
+    }
+}
+
+namespace Xunit
+{
+    public partial class Assert
+    {
+        public static void Question(TomlTable expectedQuestion, Question question)
+        {
+            Assert.Equal(expectedQuestion.Get<string>("name"), question.Name);
+            Assert.Equal(expectedQuestion.Get<Class>("class"), question.Class);
+            Assert.Equal(expectedQuestion.Get<Ward.Dns.Type>("type"), question.Type);
+        }
+
+        public static void Record(TomlTable expectedRecord, Ward.Dns.Record record)
+        {
+            Assert.Equal(expectedRecord.TryGetValue("name")?.Get<string>(), record.Name);
+            Assert.Equal(expectedRecord.Get<Ward.Dns.Type>("type"), record.Type);
+
+            var @class = expectedRecord.Get("class");
+            if (@class.TomlType == TomlObjectType.Int)
+                Assert.Equal((Class)(@class.Get<int>()), record.Class);
+            else
+                Assert.Equal(@class.Get<Class>(), record.Class);
+
+            Assert.Equal(expectedRecord.Get<uint>("ttl"), record.TimeToLive);
+            Assert.Equal(expectedRecord.Get<ushort>("length"), record.Length);
+            Assert.Equal(expectedRecord.Get<string>("data"), record.Data.Aggregate(string.Empty, (s, v) => {
+                return s += v.ToString("X2").ToLower();
+            }));
+        }
+
+        public static void Header(TomlTable expectedHeader, Header header)
         {
             Assert.Equal(expectedHeader.Get<ushort>("id"), header.Id);
             Assert.Equal(expectedHeader.Get<bool>("query"), header.Query);
--- test/Ward.Dns.Tests/HeaderTests.cs
+++ test/Ward.Dns.Tests/HeaderTests.cs
@@ -11,17 +11,17 @@ namespace Ward.Dns.Tests
     public class HeaderTests
     {
         [Theory]
-        [MemberData(nameof(BuildHeaderTests))]
+        [MemberData(nameof(GenerateHeaderTests))]
         public void Can_parse_header(TomlTable testCaseData)
         {
             var message = Convert.FromBase64String(testCaseData.Get<string>("data"));
             var header = Header.ParseFromStream(new MemoryStream(message));
             var expectedHeader = testCaseData.Get<TomlTable>("expected").Get<TomlTable>("header");
 
-            Assertions.AssertHeader(header, expectedHeader);
+            Assert.Header(expectedHeader, header);
         }
 
-        public static TheoryData<TomlTable> BuildHeaderTests()
+        public static TheoryData<TomlTable> GenerateHeaderTests()
         {
             bool TestCaseMatches(TomlTable tt) =>
                 tt.ContainsKey("expected") && tt.Get<TomlTable>("expected").ContainsKey("header");
