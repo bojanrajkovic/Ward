761d26b4254548a7c174cfa1a77ec1ce9019ab5c
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2018-04-15 11:13:32 -0400

    Tests: Move header test generation around.

--- test/Ward.Dns.Tests/HeaderTests.cs
+++ test/Ward.Dns.Tests/HeaderTests.cs
@@ -11,25 +11,13 @@ namespace Ward.Dns.Tests
     public class HeaderTests
     {
         [Theory]
-        [MemberData(nameof(GenerateHeaderTests))]
-        public void Can_parse_header(TomlTable testCaseData)
+        [MemberData(nameof(TestGenerators.GenerateHeaderTests), MemberType = typeof(TestGenerators))]
+        public void Can_parse_header(string testCaseName, byte[] messageData, TomlTable testCaseData)
         {
-            var message = Convert.FromBase64String(testCaseData.Get<string>("data"));
-            var header = Header.ParseFromStream(new MemoryStream(message));
+            var header = Header.ParseFromStream(new MemoryStream(messageData));
             var expectedHeader = testCaseData.Get<TomlTable>("expected").Get<TomlTable>("header");
 
             Assert.Header(expectedHeader, header);
-        }
-
-        public static TheoryData<TomlTable> GenerateHeaderTests()
-        {
-            bool TestCaseMatches(TomlTable tt) =>
-                tt.ContainsKey("expected") && tt.Get<TomlTable>("expected").ContainsKey("header");
-
-            var td = new TheoryData<TomlTable>();
-            foreach (var test in TestCaseLoader.FindTestCasesMatching(TestCaseMatches))
-                td.Add(test);
-            return td;
         }
     }
 }
--- test/Ward.Dns.Tests/TestGenerators.cs
+++ test/Ward.Dns.Tests/TestGenerators.cs
@@ -15,11 +15,18 @@ namespace Ward.Dns.Tests
 
             var td = new TheoryData<string, byte[], TomlTable>();
             foreach (var test in TestCaseLoader.FindTestCasesMatching(TestCaseMatches))
-                td.Add(
-                    test.Get<string>("name"),
-                    Convert.FromBase64String(test.Get<string>("data")),
-                    test
-                );
+                td.Add(test.Get<string>("name"), Convert.FromBase64String(test.Get<string>("data")), test);
+            return td;
+        }
+
+        public static TheoryData<string, byte[], TomlTable> GenerateHeaderTests()
+        {
+            bool TestCaseMatches(TomlTable tt) =>
+                tt.ContainsKey("expected") && tt.Get<TomlTable>("expected").ContainsKey("header");
+
+            var td = new TheoryData<string, byte[], TomlTable>();
+            foreach (var test in TestCaseLoader.FindTestCasesMatching(TestCaseMatches))
+                td.Add(test.Get<string>("name"), Convert.FromBase64String(test.Get<string>("data")), test);
             return td;
         }
     }
