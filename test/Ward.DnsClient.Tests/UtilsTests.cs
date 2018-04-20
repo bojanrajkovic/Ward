8264f8d6a37a289cfa547beb7cccd3cd1a637f22
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2018-04-19 23:56:28 -0400

    Tests: Mark hash test.

--- test/Ward.DnsClient.Tests/UtilsTests.cs
+++ test/Ward.DnsClient.Tests/UtilsTests.cs
@@ -5,6 +5,7 @@ namespace Ward.DnsClient.Tests
 {
     public class UtilsTests
     {
+        [Fact]
         public void Hash_throws_ANE_if_hash_null()
         {
             Assert.Throws<ArgumentNullException>(() => Utils.Hash(null, Array.Empty<byte>()));
