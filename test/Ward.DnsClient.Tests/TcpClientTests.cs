5dbc0c0b27c50e16d3fec17c4d0d44bd6243d8a0
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2018-04-20 00:00:36 -0400

    Tests: Test that bad SPKI hashes throw exceptions w/ DNS-over-TLS.

--- test/Ward.DnsClient.Tests/TcpClientTests.cs
+++ test/Ward.DnsClient.Tests/TcpClientTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net;
+using System.Security;
 using System.Threading.Tasks;
 
 using Ward.Dns;
@@ -49,6 +50,20 @@ namespace Ward.DnsClient.Tests
             var a = Assert.IsType<AddressRecord>(resolve.Results[0]);
             Assert.Equal("93.184.216.34", a.Address.ToString());
             Assert.Equal(Class.Internet, a.Class);
+        }
+
+        [Fact]
+        public async Task Bad_spki_throws_security_exception()
+        {
+            const string badSpkiHash = "72lKu9HsDVbyiPenApnc4sfmSYTHOVfFgL3pyB+cBL4=";
+
+            var client = new TcpDnsClient(IPAddress.Parse("145.100.185.15"), 853, true, "dnsovertls.sinodun.com", badSpkiHash);
+            var ex = await Assert.ThrowsAsync<SecurityException>(async () => {
+                await client.ResolveAsync("example.com", Type.A, Class.Internet);
+            });
+
+            const string goodSpkiHash = "62lKu9HsDVbyiPenApnc4sfmSYTHOVfFgL3pyB+cBL4=";
+            Assert.Equal($"SPKI hash {goodSpkiHash} did not match expected SPKI hash {badSpkiHash}", ex.Message);
         }
     }
 }
