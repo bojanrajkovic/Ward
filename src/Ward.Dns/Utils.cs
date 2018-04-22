21588330c0945fb21e1e028468d02f569d4f7044
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2018-04-21 22:59:24 -0400

    Dns: Implement name compression algorithm when writing RRs.
    This compression algorithm is a trip. Basically, the message writer
    needs to know when a QNAME needs to be written, because it needs to
    look for the longest suffix that's already been written to the message
    so that it can compress the name by only writing parts of the name
    that have not before been seen in the message.

--- src/Ward.Dns/Utils.cs
+++ src/Ward.Dns/Utils.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
@@ -48,12 +49,42 @@ namespace Ward.Dns
             return nameBuilder.ToString();
         }
 
-        public static byte[] WriteQName(string name)
+        public static byte[] WriteQName(string name, Dictionary<string, ushort> offsetMap)
         {
-            var punycode = new IdnMapping().GetAscii(name);
-            var labels = punycode.Split('.');
-            var qname = new byte[labels.Sum(l => 1+l.Length) + 1];
+            if (string.IsNullOrWhiteSpace(name))
+                return new byte[] { 0 };
 
+            offsetMap = offsetMap ?? new Dictionary<string, ushort>();
+
+            // Convert the name to Punycode.
+            name = new IdnMapping().GetAscii(name);
+
+            // Find the longest name that's a suffix of this one
+            string longestSuffix = null;
+            foreach (var existingName in offsetMap.Keys) {
+                if (!name.EndsWith(existingName))
+                    continue;
+
+                if (existingName.Length > (longestSuffix?.Length ?? 0))
+                    longestSuffix = existingName;
+            }
+
+            // Strip the longest suffix from the end of the name.
+            name = name.Substring(0, name.Length - (longestSuffix?.Length ?? 0));
+            var offset = (ushort)(longestSuffix != null ? (0b1100_0000_0000_0000 | offsetMap[longestSuffix]) : 0);
+
+            var labels = name.Split(new [] { '.', }, StringSplitOptions.RemoveEmptyEntries);
+            var qnameLength = labels.Sum(l => 1+l.Length);
+
+            // If the offset is non-zero, append 2 bytes to the length of the labels.
+            // We'll write the ASCII labels, and then the 2 byte offset. If the offset is 0,
+            // we're writing the entire string, so we only need to add 1 byte to the length,
+            // for the null terminator.
+            qnameLength += offset == 0 ? 1 : 2;
+
+            var qname = new byte[qnameLength];
+
+            // Write any labels to the output.
             var pos = 0;
             foreach (var label in labels) {
                 qname[pos++] = (byte)label.Length;
@@ -65,6 +96,11 @@ namespace Ward.Dns
                     label.Length
                 );
                 pos += label.Length;
+            }
+
+            if (offset != 0) {
+                var offsetBytes = BitConverter.GetBytes(SwapUInt16(offset));
+                Buffer.BlockCopy(offsetBytes, 0, qname, pos, offsetBytes.Length);
             }
 
             return qname;
