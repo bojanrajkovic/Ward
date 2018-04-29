5c7cdf8ee0ca365aea2451f5aeb7b60c8c873519
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2018-04-29 15:07:28 -0400

    Tests: Test OPT writing/RCODE fixup.

--- test/Ward.Dns.Tests/MessageWriterTests.cs
+++ test/Ward.Dns.Tests/MessageWriterTests.cs
@@ -23,5 +23,37 @@ namespace Ward.Dns.Tests
             var recordData = fakeStream.ToArray();
             Assert.Equal(testCase.ExpectedData, recordData);
         }
+
+        [Fact]
+        public async Task Extended_rcode_is_serialized_correctly() {
+            var flags = new Header.HeaderFlags(true, false, false, true, true, false, false, false);
+            var header = new Header(null, Opcode.Query, ReturnCode.BadTrunc, flags, 1, 0, 0, 0);
+            var question = new Question("google.com.", Type.A, Class.Internet);
+            var message = new Message(header, new [] { question }, Array.Empty<Record>(), Array.Empty<Record>(), Array.Empty<Record>());
+            var serializedMessage = await MessageWriter.SerializeMessageAsync(message);
+
+            var parsedMessage = MessageParser.ParseMessage(serializedMessage, 0);
+
+            Assert.NotNull(parsedMessage);
+
+            // The parser should have automatically fixed up the return code in the header.
+            Assert.Equal(ReturnCode.BadTrunc, parsedMessage.Header.ReturnCode);
+
+            // But if we inspect the bytes, we should find that only the 2nd and 3rd are set
+            // in the actual header RCODE field.
+            var flagsBitfield = Utils.SwapUInt16(BitConverter.ToUInt16(serializedMessage, 2));
+            var returnCode = (ReturnCode)(flagsBitfield & 0b0000_0000_0000_1111);
+            Assert.Equal((ReturnCode)0b0000_0110, returnCode);
+
+            // The message writer should have automatically inserted an OPT record with
+            // correct values.
+            Assert.Equal(1, parsedMessage.Header.TotalAdditionalRecords);
+            Assert.Single(parsedMessage.Additional);
+            var optRecord = Assert.IsType<OptRecord>(parsedMessage.Additional[0]);
+            Assert.Equal(1, optRecord.ExtendedRcode);
+            Assert.Equal(0, optRecord.Edns0Version);
+            Assert.Equal(4096, optRecord.UdpPayloadSize);
+            Assert.Equal(0, optRecord.Length);
+        }
     }
 }
