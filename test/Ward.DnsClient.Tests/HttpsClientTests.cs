c96a3e0cc8214796a89f363177cb6dc3a32c99e1
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2018-05-19 00:05:32 -0400

    Tests: Clean up tests for API changes.

--- test/Ward.DnsClient.Tests/HttpsClientTests.cs
+++ test/Ward.DnsClient.Tests/HttpsClientTests.cs
@@ -12,7 +12,7 @@ namespace Ward.DnsClient.Tests
         [Fact]
         public async Task Can_resolve_A_records_via_HTTPS()
         {
-            var client = new HttpsDnsClient(IPAddress.Parse("1.1.1.1"), 443, "cloudflare-dns.com");
+            var client = new HttpsDnsClient("1.1.1.1", 443, "cloudflare-dns.com");
             var resolve = await client.ResolveAsync("example.com", Type.A, Class.Internet);
 
             Assert.NotNull(resolve);
@@ -20,7 +20,6 @@ namespace Ward.DnsClient.Tests
             Assert.Empty(resolve.Authority);
             Assert.Empty(resolve.Additional);
             Assert.Single(resolve.Questions);
-            Assert.NotNull(resolve.Header);
             Assert.True(resolve.MessageSize > 0);
 
             var a = Assert.IsType<AddressRecord>(resolve.Answers[0]);
@@ -31,7 +30,7 @@ namespace Ward.DnsClient.Tests
         [Fact]
         public async Task Can_resolve_A_records_via_HTTPS_with_SPKI()
         {
-            var client = new HttpsDnsClient(IPAddress.Parse("1.1.1.1"), 443, "cloudflare-dns.com", "yioEpqeR4WtDwE9YxNVnCEkTxIjx6EEIwFSQW+lJsbc=");
+            var client = new HttpsDnsClient("1.1.1.1", 443, "cloudflare-dns.com", "yioEpqeR4WtDwE9YxNVnCEkTxIjx6EEIwFSQW+lJsbc=");
             var resolve = await client.ResolveAsync("example.com", Type.A, Class.Internet);
 
             Assert.NotNull(resolve);
@@ -39,7 +38,6 @@ namespace Ward.DnsClient.Tests
             Assert.Empty(resolve.Authority);
             Assert.Empty(resolve.Additional);
             Assert.Single(resolve.Questions);
-            Assert.NotNull(resolve.Header);
             Assert.True(resolve.MessageSize > 0);
 
             var a = Assert.IsType<AddressRecord>(resolve.Answers[0]);
