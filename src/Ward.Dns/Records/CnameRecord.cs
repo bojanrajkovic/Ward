78cb08fd68c5fa41b569aa2a5c26dc57800c68c8
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2018-05-24 02:01:56 +0000

    Docs: Document CNAME record.

--- src/Ward.Dns/Records/CnameRecord.cs
+++ src/Ward.Dns/Records/CnameRecord.cs
@@ -4,10 +4,21 @@ using static Ward.Dns.Utils;
 
 namespace Ward.Dns.Records
 {
+    /// <summary>
+    /// A CNAME record.
+    /// </summary>
     public class CnameRecord : Record
     {
+        /// <summary>The canonical or primary name for the owner.</summary>
+        /// <returns>The canonical or primary name for the owner.</returns>
         public string Hostname { get; }
 
+        /// <summary>
+        /// Creates a CNAME record.
+        /// </summary>
+        /// <remarks>
+        /// Only used from internal parsing code.
+        /// </remarks>
         internal CnameRecord(
             string name,
             Class @class,
@@ -20,6 +31,13 @@ namespace Ward.Dns.Records
             Hostname = ParseComplexName(message, data.ToArray(), ref _);
         }
 
+        /// <summary>
+        /// Creates a CNAME record.
+        /// </summary>
+        /// <param name="name">The owner-name (or label) to which this record belongs.</param>
+        /// <param name="class">The record class.</param>
+        /// <param name="timeToLive">The record time to live.</param>
+        /// <param name="cname">The canonical or primary name for the owner.</param>
         public CnameRecord(
             string name,
             Class @class,
@@ -29,6 +47,10 @@ namespace Ward.Dns.Records
             Hostname = cname;
         }
 
+        /// <summary>
+        /// Converts the current record to a string.
+        /// </summary>
+        /// <returns>A string version of the current record.</returns>
         [System.Diagnostics.DebuggerStepThrough]
         public override string ToString() =>
             $"{Name}\t{TimeToLive}\t{Class}\t{Type}\t{Hostname}";
