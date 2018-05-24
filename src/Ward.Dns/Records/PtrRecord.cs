ee41dad5eda5e0e143b6a5d4b6f3f829aa98a4a6
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2018-05-24 02:47:40 +0000

    Docs: Document PTR record.

--- src/Ward.Dns/Records/PtrRecord.cs
+++ src/Ward.Dns/Records/PtrRecord.cs
@@ -4,10 +4,23 @@ using static Ward.Dns.Utils;
 
 namespace Ward.Dns.Records
 {
+    /// <summary>
+    /// A PTR record.
+    /// </summary>
     public class PtrRecord : Record
     {
+        /// <summary>
+        /// A domain-name which points to some location in the domain name space.
+        /// </summary>
+        /// <returns>A domain-name.</returns>
         public string Hostname { get; }
 
+        /// <summary>
+        /// Creates a PTR record.
+        /// </summary>
+        /// <remarks>
+        /// Only used from internal parsing code.
+        /// </remarks>
         internal PtrRecord(
             string name,
             Class @class,
@@ -20,6 +33,13 @@ namespace Ward.Dns.Records
             Hostname = ParseComplexName(message, data.ToArray(), ref _);
         }
 
+        /// <summary>
+        /// Creates a PTR record.
+        /// </summary>
+        /// <param name="name">The owner-name (or label) to which this record belongs.</param>
+        /// <param name="class">The record class.</param>
+        /// <param name="timeToLive">The record time to live.</param>
+        /// <param name="hostname">A domain-name in the domain name space to point at.</param>
         public PtrRecord(
             string name,
             Class @class,
@@ -29,6 +49,10 @@ namespace Ward.Dns.Records
             Hostname = hostname;
         }
 
+        /// <summary>
+        /// Converts the current record to a string.
+        /// </summary>
+        /// <returns>A string version of the current record.</returns>
         [System.Diagnostics.DebuggerStepThrough]
         public override string ToString() => $"{Name}\t{TimeToLive}\t{Class}\t{Type}\t{Hostname}";
     }
