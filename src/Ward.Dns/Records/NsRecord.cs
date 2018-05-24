5afc28dcda1d685c19af3d07db102ca3d1659a9f
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2018-05-24 02:11:56 +0000

    Docs: Document NS record.

--- src/Ward.Dns/Records/NsRecord.cs
+++ src/Ward.Dns/Records/NsRecord.cs
@@ -4,10 +4,24 @@ using static Ward.Dns.Utils;
 
 namespace Ward.Dns.Records
 {
+    /// <summary>
+    /// An NS record.
+    /// </summary>
     public class NsRecord : Record
     {
+        /// <summary>
+        /// A domain-name which specifies a host which should be authoritative
+        /// for the specified class and domain.
+        /// </summary>
+        /// <returns>A domain-name.</returns>
         public string Hostname { get; }
 
+        /// <summary>
+        /// Creates an NS record.
+        /// </summary>
+        /// <remarks>
+        /// Only used from internal parsing code.
+        /// </remarks>
         internal NsRecord(
             string name,
             Class @class,
@@ -20,6 +34,16 @@ namespace Ward.Dns.Records
             Hostname = ParseComplexName(message, data.ToArray(), ref _);
         }
 
+        /// <summary>
+        /// Creates an NS record.
+        /// </summary>
+        /// <param name="name">The owner-name (or label) to which this record belongs.</param>
+        /// <param name="class"The record class.</param>
+        /// <param name="timeToLive">The record time to live.</param>
+        /// <param name="hostname">
+        /// A domain-name which specifies a host which should be authoritative
+        /// for the specified class and domain.
+        /// </param>
         public NsRecord(
             string name,
             Class @class,
@@ -29,6 +53,10 @@ namespace Ward.Dns.Records
             Hostname = hostname;
         }
 
+        /// <summary>
+        /// Converts the current record to a string.
+        /// </summary>
+        /// <returns>A string version of the current record.</returns>
         [System.Diagnostics.DebuggerStepThrough]
         public override string ToString() => $"{Name}\t{TimeToLive}\t{Class}\t{Type}\t{Hostname}";
     }
