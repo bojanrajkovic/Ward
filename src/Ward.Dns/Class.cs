a827a237a1ff2813416295fdf885df5188b7c1af
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2018-06-03 10:07:39 -0400

    Docs: Document Class.

--- src/Ward.Dns/Class.cs
+++ src/Ward.Dns/Class.cs
@@ -1,5 +1,13 @@
 namespace Ward.Dns
 {
+    /// <summary>
+    /// A DNS RR CLASS/QCLASS.
+    /// </summary>
+    /// <remarks>
+    /// The first four values appear only in responses. <see cref="Class.Any">
+    /// is a QCLASS value and only appears in questions, but is included
+    /// here for convenience.
+    /// </remarks>
     public enum Class : ushort
     {
         Internet = 1,
