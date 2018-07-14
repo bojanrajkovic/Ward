3e95d092fc1883702b953a1d9b968151ead89bdd
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2018-07-14 10:30:07 -0400

    DNS Server: Scaffold DNS server project.

---
+++ src/Ward.DnsServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ward.DnsServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
